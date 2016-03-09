20fdcc29281791aaa7792c01658e45a48e6064c6
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-03-09 10:51:45 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ import json
 import time
 from pprint import pprint
 
-API_TOKEN = 'token'
+API_TOKEN = '200157869:AAEv57P9MWb8w_X9ga86Kvslse9wnQ3CQP0'
 bot = telebot.TeleBot(API_TOKEN)
 
 with open('loggingids.json') as f:
@@ -34,13 +34,13 @@ def on_user_joins(m):
 				user.append(str(cid))
 				with open('chat.txt', 'a') as f:
 					f.write(str(cid)+"\n")
-			bot.send_message(cid, "Hi ty for invite me but now im not work in group í ½í¸‰\nIf you want to see all the cute guys behind the Bot type /credits í ½í¸‡")
+			bot.send_message(cid, "Hi ty for invite me but now im not work in group ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\nIf you want to see all the cute guys behind the Bot type /credits ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½")
 			print "New group received."
 			userwhogotadded = m.new_chat_participant.first_name
 			username = m.new_chat_participant.username
 			groupname = m.chat.title
 			groupid = m.chat.id
-			bot.send_message('-84875656', "# DEBUG # " + "Bot got invited to the group " + str(groupname) + "(" + str(groupid) + ")", parse_mode="HTML")
+			bot.send_message('141725601', "# DEBUG # " + "Bot got invited to the group " + str(groupname) + "(" + str(groupid) + ")", parse_mode="HTML")
 
 
 
@@ -62,7 +62,7 @@ def ask(m):
     str = m.text
     txt = str.replace("/ask", "")
     bot.send_message(idA, "<b>Thank you very much! Details to your Question:\n Quetsion:</b> {} \n<b>Chat-Id :</b> {} \n<b>We will answer you as soon as possible</b>".format(txt,idA), parse_mode="HTML")
-    bot.send_message('-84875656', "<b>#NEW MSG\n Chat-Id:</b> {} \n<b>Msg :</b> {}".format(idA,txt), parse_mode="HTML")
+    bot.send_message('141725601', "<b>#NEW MSG\n Chat-Id:</b> {} \n<b>Msg :</b> {}".format(idA,txt), parse_mode="HTML")
     bot.send_message(idA, "<b>This is a confirmation that we received your Question</b>", parse_mode="HTML")
 
 
